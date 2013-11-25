79070f9deb2a154b08b243ae34330c38347fa313
ranyefet <ran@yefet.net>
2013-11-25 17:24:44 +0200

    Default language can be configured

--- Lang.php
+++ Lang.php
@@ -2,32 +2,40 @@
 
 class Lang {
 
-	const DEFAULT_LANG = 'en';
-
 	var $messages = array();
 
 	var $options = array(
+		'default' => 'en',
 		'directory' => 'languages',
 		'ext' => 'json'
 	);
 
 	function __construct( $language = null, $options = array() ){
-
-		if( !$language )
-			throw new Exception('Error class Lang excepts $language as first parameter');
 			
+		// Merge options with defaults
 		$this->options = array_merge($this->options, $options);
 
+		// If we didn't get a language use default
+		if( !$language ){
+			$language = $this->options['default'];
+		}
+
 		// If the current language does not exists and we have underscore in the name
-		// Strip the country suffix
+		// Strip the country suffix en_US
 		if( !$this->exists( $language ) && strpos($language, "_") !== false ){
 			$language = substr( $language, 0, 2);
 		}
+		
 		// If we still don't have language, use the default one
 		if( !$this->exists($language) ){
-			$language = self::DEFAULT_LANG;
+			$language = $this->options['default'];
 		}
-		$this->load( $language );
+
+		if( $this->exists($language) ){
+			$this->load( $language );
+		} else {
+			throw new Exception("Language file: " . $this->path($language) . " not found!");
+		}
 	}
 
 	function exists( $language ){
