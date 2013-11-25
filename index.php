dfd0f847f798cffa307f6deffa3e38066bd2c041
ranyefet <ran@yefet.net>
2013-11-25 17:25:52 +0200

    Switch all hard-coded strings to use translate method

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $clipperFlashvars .= '&ks=' . $ks . '&show_add_delete_buttons=false&state=clippi
 $clipperFlashvars .= '&max_allowed_rows=1&show_control_bar=true&show_message_box=false&protocol=' . $protocol . '://';
 
 if(!$entryId)
-	die("Missing entry id");
+	die(t("Entry Id not found"));
 
 // Load entry
 try
@@ -34,11 +34,11 @@ catch(Exception $e)
 
 if( $conf['overwrite_entry'] ){
 	$save_message = $conf['trim_save_message'];
-	$form_title = 'Use the trimming timeline below or enter exact in and out times';
-	$trim_note = '<p class="note">Note: Trimming will replace your original media!</p>';
+	$form_title = t('Use the trimming timeline below or enter exact in and out times');
+	$trim_note = '<p class="note">' . t('Note: Trimming will replace your original media!') . '</p>';
 } else {
 	$save_message = $conf['clip_save_message'];
-	$form_title = '<a href="#">Add New Clip</a>';
+	$form_title = '<a href="#">' . t('Add New Clip') . '</a>';
 	$trim_note = '';
 }
 
@@ -53,6 +53,11 @@ $clipAppConfig = array(
 	"redirect_save" => $conf['redirect_save'],
 	"redirect_url" => $conf['redirect_url'],
 	"overwrite_entry" => $conf['overwrite_entry'],
+	"messages" => array(
+		"start_time_error" => t('End-time must be larger than Start-time'),
+		"clip_duration_error" => t('Clip duration must be at least 100ms'),
+		"delete_confirmation" => t('Are you sure you want to delete?'),
+	)
 );
 
 ?>
@@ -74,14 +79,22 @@ $clipAppConfig = array(
 <!--[if IE 9 ]><body class="ie ie9"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--><body><!--<![endif]-->
 		<div id="wrapper">
-			<object id="kdp3" name="kdp3" type="application/x-shockwave-flash" wmode="window" allowFullScreen="true" allowNetworking="all" allowScriptAccess="always" bgcolor="#000000" resource="<?php echo $kdpUrl; ?>" data="<?php echo $kdpUrl; ?>"><param name="allowFullScreen" value="true" /><param name="allowNetworking" value="all" /><param name="allowScriptAccess" value="always" /><param name="wmode" value="window" /><param name="bgcolor" value="#000000" /><param name="flashVars" value="ks=<?php echo $ks;?>" /><param name="movie" value="<?php echo $kdpUrl; ?>" /></object>
+			<object id="kdp3" name="kdp3" type="application/x-shockwave-flash" wmode="window" allowFullScreen="true" allowNetworking="all" allowScriptAccess="always" bgcolor="#000000" data="<?php echo $kdpUrl; ?>">
+				<param name="allowFullScreen" value="true" />
+				<param name="allowNetworking" value="all" />
+				<param name="allowScriptAccess" value="always" />
+				<param name="wmode" value="window" />
+				<param name="bgcolor" value="#000000" />
+				<param name="flashVars" value="ks=<?php echo $ks;?>" />
+				<param name="movie" value="<?php echo $kdpUrl; ?>" />
+			</object>
 			<div id="form" class="form clearfix">
-				<div id="newclip"><div class="disable"></div><img id="loader" src="images/loader.gif" alt="Saving..." /><?php echo $form_title; ?></div>
+				<div id="newclip"><div class="disable"></div><img id="loader" src="images/loader.gif" alt="<?php echo t('Saving'); ?>..." /><?php echo $form_title; ?></div>
 				<div id="embed" class="form clearfix">
 					<p><?php echo $save_message; ?></p><br />
 					<?php if( $conf['show_embed'] === true ) { ?>
 					<div class="item clearfix">
-						<label>Embed:</label>
+						<label><?php echo t('Embed'); ?>:</label>
 						<input id="embedcode" class="text-field" type="text" value="" />
 					</div><br />
 					<?php } ?>
@@ -90,42 +103,49 @@ $clipAppConfig = array(
 					<?php echo $trim_note; ?>
 					<div class="disable"></div>
 					<div class="item clearfix">
-						<label>Start Time:</label>
+						<label><?php echo t('Start Time'); ?>:</label>
 						<input id="startTime" value="" />
 					</div>
 					<div class="item clearfix">
-						<label>End Time:</label>
+						<label><?php echo t('End Time'); ?>:</label>
 						<input id="endTime" value="" />
 					</div>
 					<?php if( ! $conf['overwrite_entry'] ): ?>
 					<div class="item clearfix">
-						<label>Title:</label>
+						<label><?php echo t('Title'); ?>:</label>
 						<input id="entry_title" class="text-field" type="text" value="<?php echo htmlspecialchars($entry->name); ?>" /><br /><br />
 					</div>
 					<div class="item clearfix">
-						<label>Description:</label>
+						<label><?php echo t('Description'); ?>:</label>
 						<textarea id="entry_desc"><?php echo htmlspecialchars($entry->description); ?></textarea><br /><br />
 					</div>
 					<?php endif; ?>
 				</div>
 			</div>
-			<object id="clipper" name="clipper" type="application/x-shockwave-flash" wmode="window" allowNetworking="all" allowScriptAccess="always" data="<?php echo $clipperUrl; ?>"><param name="allowNetworking" value="all" /><param name="allowScriptAccess" value="always" /><param name="wmode" value="window" /><param name="bgcolor" value="#f8f8f8" /><param name="flashVars" value="<?php echo $clipperFlashvars; ?>" /><param name="movie" value="<?php echo $clipperUrl; ?>" /></object>
+			<object id="clipper" name="clipper" type="application/x-shockwave-flash" wmode="window" allowNetworking="all" allowScriptAccess="always" data="<?php echo $clipperUrl; ?>">
+				<param name="allowNetworking" value="all" />
+				<param name="allowScriptAccess" value="always" />
+				<param name="wmode" value="window" />
+				<param name="bgcolor" value="#f8f8f8" />
+				<param name="flashVars" value="<?php echo $clipperFlashvars; ?>" />
+				<param name="movie" value="<?php echo $clipperUrl; ?>" />
+			</object>
 			
 			<div id="actions" class="clearfix">
 				<div class="disable"></div>
 				<div class="left clearfix">
-					<a href="#" id="setStartTime">Set In</a>
-					<a href="#" id="setEndTime">Set Out</a>
+					<a href="#" id="setStartTime"><?php echo t('Set In'); ?></a>
+					<a href="#" id="setEndTime"><?php echo t('Set Out'); ?></a>
 				</div>
 				<div class="right clearfix">
-					<a href="#" id="preview">Preview</a>
+					<a href="#" id="preview"><?php echo t('Preview'); ?></a>
 					<span class="seperator"> | </span>
-					<a href="#" id="delete">Remove</a>
+					<a href="#" id="delete"><?php echo t('Remove'); ?></a>
 				</div>
 			</div>
 			<div id="save">
 				<div class="disable"></div>
-				<a href="#">Save</a>
+				<a href="#"><?php echo t('Save'); ?></a>
 			</div>
 		</div>
 	</body>
